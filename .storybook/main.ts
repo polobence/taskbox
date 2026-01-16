98ccb09d5a757f66f18da35d5a27e0b67ff2f70d
polobence <polovicsbence@gmail.com>
2026-01-16 12:11:06 +0100

    build the first component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,9 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.@(ts|tsx)"],
   staticDirs: ["../public"],
-  addons: [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-vitest",
-  ],
+  addons: ["@chromatic-com/storybook", "@storybook/addon-docs", "@storybook/addon-vitest"],
   framework: {
     name: "@storybook/react-vite",
     options: {},

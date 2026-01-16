f3e461e17a596e8240d0462667ea529d1d1917f5
polobence <polovicsbence@gmail.com>
2026-01-16 14:23:51 +0100

    change color in list item

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,12 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/components/**/*.stories.@(ts|tsx)"],
   staticDirs: ["../public"],
-  addons: ["@chromatic-com/storybook", "@storybook/addon-docs", "@storybook/addon-vitest"],
+  addons: [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y"
+  ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
